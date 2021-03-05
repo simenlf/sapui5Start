3ac90266c18925eebdac775cba1827b253f34615
Simen Larsen-Frivoll <simenlf@gmail.com>
2021-03-05 11:19:39 +0000

    smaller

--- webapp/modules/detail/detail.controller.js
+++ webapp/modules/detail/detail.controller.js
@@ -11,24 +11,8 @@ sap.ui.define(["voestalpine/sapui5Start/lib/BaseController","voestalpine/sapui5S
 			
 			onInit: function () {
 			    this.getRouter().getRoute("detail").attachPatternMatched(this.startDetail, this);
-            },
-            
-            startDetail: function(oEvent) {
-                var sId = oEvent.getParameter("arguments").id;
-                
-                if (!sId){
-                    this.getRouter().navTo("start"); 
-                } else {
-                     this.getData(
-                         "ProdOrderSet",
-                         { filter: "OrderID eq '" + sId + "'", expand: "ProdOrderToProdOrderItem"}
-                         ).then((oData) => this.onDataReceived(oData));
-                }
-            },
-            
-            onDataReceived: function(oData){
-                this.getView().setModel(this.makeModel(oData.results[0]));
-                console.log("Received data detail page",oData.results[0]);
             }
+            
+            
 		});
 	});
--- webapp/modules/start/start.controller.js
+++ webapp/modules/start/start.controller.js
@@ -11,27 +11,6 @@ sap.ui.define(["voestalpine/sapui5Start/lib/BaseController","voestalpine/sapui5S
 			
 			onInit: function () {
 			    this.getRouter().getRoute("start").attachPatternMatched(this.getInitialData(), this);
-            },
-            
-            getInitialData: function(){
-                this.getData("ProdOrderSet").then(
-                    (oData) => this.onDataReceived(oData));
-            },
-            
-            // events
-            
-            onDataReceived: function(oData){
-                this.getView().setModel(this.makeModel(oData.results));
-                
-                console.log("Received data start page",oData.results);
-            },
-            
-            onRowSelect: function(oEvent){
-                var rowContext  = oEvent.getParameter("rowContext");
-                var sPath       = rowContext.sPath;
-                var sOrder      = rowContext.oModel.getProperty(sPath).OrderID;
-                
-                this.getRouter().navTo("detail",{id: sOrder});
             }
 		});
 	});
